13f2b841570f4254dad812dca3f07778e20b72a8
Zsolt Harmath <harmathzs@gmail.com>
2026-09-17 13:56:04 +0200

    Establish connection between backend and MySQL

--- backend-on-mysql.js
+++ backend-on-mysql.js
@@ -4,7 +4,28 @@ const app = express()
 
 app.use(express.json())
 
+const conn = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "users"
+})
+conn.connect(err => {
+    if (err) {
+        console.warn("Can't connect to MySQL database! Error: ", err.message)
+    } else {
+        console.log("Connected to MySQL.")
+    }
+})
 
+/**
+ * GET /users
+ */
+app.get("/users", (req, res)=>{
+    const data = {}
+    return res.status(200).json(data)
+})
 
 const PORT = 3000
 app.listen(PORT, ()=>{
